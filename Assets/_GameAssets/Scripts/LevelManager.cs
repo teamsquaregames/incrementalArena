4c428ad798df0727efad6117491372ac87dfff98
Martin Rossel <martin@pinpinteam.com>
2026-03-20 10:08:57 +0100

    auto attack crossfade

--- Assets/_GameAssets/Scripts/LevelManager.cs
+++ Assets/_GameAssets/Scripts/LevelManager.cs
@@ -4,17 +4,27 @@ using Lean.Pool;
 using MyBox;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Utils;
 using Random = UnityEngine.Random;
 
 public class LevelManager : Singleton<LevelManager>
 {
+    [SerializeField] private EntityManager m_entityManager = EntityManager.Instance;
+
     public Entity m_playerPrefab;
     public Entity m_enemyPrefab;
     public int m_enemyCount = 5;
+    [SerializeField] private bool m_useConstantEnemyCount = true;
 
     private void Start()
     {
         //spawn player
+
+        if (m_useConstantEnemyCount)
+        {
+            EntityManager.Instance.onEntityUnregistered += ConstantEnemyCount;
+        }
+
         LeanPool.Spawn(m_playerPrefab);
         for (int i = 0; i < m_enemyCount; i++)
         {
@@ -22,8 +32,9 @@ public class LevelManager : Singleton<LevelManager>
         }
     }
 
-    private void StartRound()
+    private void ConstantEnemyCount(Entity entity)
     {
-
+        this.Log("Enemy died, spawning a new one to keep the count constant");
+        LeanPool.Spawn(m_enemyPrefab, new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 5)), Quaternion.identity);
     }
-} 
+}
